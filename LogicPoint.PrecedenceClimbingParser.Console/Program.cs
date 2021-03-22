6418bafb452b86337b16de6bb6e60fec7da5206c
Adam Kimberley <adam.j.kimberley@outlook.com>
2021-03-22 22:13:32 +0000

    add simple console app to demonstrae parser and printer

---
+++ LogicPoint.PrecedenceClimbingParser.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using static LogicPoint.PrecedenceClimbingParser.Parser;
+using static LogicPoint.PrecedenceClimbingParser.Printing.Printer;
+namespace LogicPoint.PrecedenceClimbingParser.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Console.WriteLine("Please enter arithmetic expression to parse...");
+                var input = System.Console.ReadLine();
+                var tree = GenerateTreeFor(input);
+                var printedTree = Print(tree);
+                System.Console.Clear();
+                System.Console.WriteLine(printedTree);
+                System.Console.ReadKey();
+            }
+        }
+
+        private static INode GenerateTreeFor(string input)
+            => Parse(input);
+    }
+}
